3442a36689e0401db2f276e9892ca93bb3ec99b7
eu-pedro-miguel <opedromiguel.sc@gmail.com>
2025-11-18 22:21:32 -0300

    Add files via upload

---
+++ estrutura/navbar.php
@@ -0,0 +1,14 @@
+<nav class="navbar navbar-expand-lg bg-yellow-200">
+    <div class="container-fluid">
+        <a class="navbar-brand fs-4 text-dark css-navbar-titulo" href="#">Mercado Vila de Ouro</a>
+        <form class="d-flex ms-auto" role="search">
+            <input class="form-control me-2 mt-2 border-2 border-warning focus:ring-4 focus:ring-yellow-400" type="search" placeholder="Pesquise" aria-label="Search"/ style="width: 17.0rem">
+            <button class="btn btn-outline-success me-2 mt-2 p-2 text-light bg-green-600 hover:bg-green-700" type="submit"><i class="bi bi-search"></i></button>
+        </form>
+        <button class="btn btn-md me-2 mt-2 btn-outline-primary text-light bg-blue-500 hover:bg-blue-600" type="submit">Entrar</button>
+        <button class="btn btn-md me-2 mt-2 btn-outline-primary text-light bg-blue-500 hover:bg-blue-600" type="submit">Cadastrar</button>
+        <button type="button" class="btn btn-md me-2 mt-2 btn-outline-dark text-light bg-dark transition transform hover:scale-105" onclick="toggleTheme()">
+            Alternar Tema
+        </button>
+    </div>
+</nav>
